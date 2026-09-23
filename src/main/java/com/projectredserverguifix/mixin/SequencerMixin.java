004c3eafaf1715f901b2030a8996cf645d33e169
zachsem <4555123+zachsem@users.noreply.github.com>
2026-09-23 16:20:08 -0500

    Add patch source code

---
+++ src/main/java/com/projectredserverguifix/mixin/SequencerMixin.java
@@ -0,0 +1,25 @@
+package com.projectredserverguifix.mixin;
+
+import com.projectredserverguifix.ProjectRedGuiPacketBridge;
+import mrtjp.projectred.integration.part.GatePart;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(targets = "mrtjp.projectred.integration.part.ComplexGatePart$Sequencer", remap = false)
+public abstract class SequencerMixin {
+
+    @Redirect(
+            method = "gateLogicActivate",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lmrtjp/projectred/integration/gui/screen/TimerScreen;open(Lnet/minecraft/entity/player/PlayerEntity;Lmrtjp/projectred/integration/part/GatePart;)V",
+                    remap = false
+            ),
+            remap = false
+    )
+    private void projectRedServerGuiFix$openTimerGui(PlayerEntity player, GatePart part) {
+        ProjectRedGuiPacketBridge.openTimerGui(player, part);
+    }
+}
